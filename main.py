b690b18060cca1f7a7e588cf343750a8a9a0a8a7
afterhty <102747828+afterhty@users.noreply.github.com>
2022-04-05 18:42:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,51 @@ from telebot import types
 
 import telebot
 
-TOKEN = 'ВАШ ТОКЕН'
+TOKEN = ''5178549233:AAFzGJ_ilc5yFKMwvRmZYerFeoYOWsQ_KEg'
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 
 #КОД ТЕЛЕБОТА
+!pip install pyTelegramBotAPI 
+
+import telebot # импортируем библиотеку для создания бота
+ 
+bot = telebot.TeleBot('5178549233:AAFzGJ_ilc5yFKMwvRmZYerFeoYOWsQ_KEg')
+ 
+name = ''
+surname = ''
+age = 0
+ 
+@bot.message_handler(content_types=['text'])
+def start(message):
+    if message.text == '/reg':
+        bot.send_message(message.from_user.id, "Как тебя зовут?")
+        bot.register_next_step_handler(message, get_name) #следующий шаг – функция get_name
+    else:
+        bot.send_message(message.from_user.id, 'Напиши /reg')
+ 
+def get_name(message): #получаем имя
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?')
+    bot.register_next_step_handler(message, get_surname)
+ 
+def get_surname(message):
+    global surname
+    surname = message.text
+    bot.send_message(message.from_user.id, 'Сколько тебе лет?')
+    bot.register_next_step_handler(message, get_age)
+ 
+def get_age(message):
+    global age
+    age = int(message.text)
+    bot.send_message(message.from_user.id, 'Тебе '+str(age)+' лет, тебя зовут '+name+' '+surname+'?')
+ 
+# запуск
+bot.polling(none_stop=True)
+
+
 
 
 @server.route('/' + TOKEN, methods=['POST'])
@@ -23,7 +62,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://<Название_вашего_хероку_приложения>.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url='https://<qweyuuiu>.herokuapp.com/' + TOKEN)
     return "!", 200
 
 if __name__ == "__main__":
